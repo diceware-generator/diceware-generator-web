96cfb21db9c5e901598d69734241d3101e00045e
Victor Lima <vrlima75@gmail.com>
2021-08-31 09:19:37 -0300

    changing path

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def index():
     return render_template('index.html', message=diceware_password)
 
 def get_diceware():
-    w = [s.split()[0] for s in open(sys.path[0]+'/dict/dicionario-pt-low.txt')]
+    w = 'dict/dicionario-pt-low.txt')]
     return ' '.join(random.SystemRandom().choice(w) for i in range(6))
